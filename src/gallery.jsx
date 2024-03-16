e08cbcbf321d327767866269abeda13ff9207b33
Bella Moss <152352216+bellamoss77@users.noreply.github.com>
2024-03-15 23:34:31 -0400

    Initial commit

---
+++ src/gallery.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+    {
+        original: '<>'
+    }
+]
+
+function App() {
+    return (
+        <div className="App">
+            <ImageGallery />
+        </div>
+    );
+}
+
+export default App;
