55b5df4b0dd24f68452e755b1defac6e26161297
Bella Moss <152352216+bellamoss77@users.noreply.github.com>
2024-03-16 01:52:11 -0400

    add thumbnail images

--- src/gallery.jsx
+++ src/gallery.jsx
@@ -3,8 +3,75 @@ import ImageGallery from 'react-image-gallery';
 
 const images = [
     {
-        original: '<>'
-    }
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG1.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG2.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG3.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG4.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG5.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG6.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG7.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG8.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG9.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG10.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG11.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG12.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG13.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG14.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG15.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG16.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG17.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG18.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG19.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG20.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG21.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG22.png?raw=true>',
+        thumbnail: '<>'
+    }, {
+        original: '<https://github.com/bellamoss77/ReactGallery/blob/main/public/PLEASANT_GROVE_MIN/PG23.png?raw=true>',
+        thumbnail: '<>'
+    },
 ]
 
 function App() {
